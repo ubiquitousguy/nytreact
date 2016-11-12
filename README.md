ae4681e74d868810cdecaf05c702497f373db32e
Guy <guypham@hotmail.com>
2016-11-11 19:41:17 -0800

    completion of application

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nytreact
+React-based rendition of the New York Times Article Search application. React components, work with helper functions, and utilize the React mounting lifecycle to query and display articles based on user searches. You'll also use Node, Express and MongoDB so that users can save articles to read later.
---
+++ app/utils/helpers.js
@@ -0,0 +1,66 @@
+/*Include the Axios library for HTTP requests*/
+var axios = require('axios');
+
+/* NYT API Key*/
+var APIKey = "9d4a8986921972b65754ea0809d47c84:12:74623931";
+// Helper Functions (in this case the only one is runQuery)
+var helpers = {
+  // This will run our query.
+  	runQuery: function(term, start, end){
+      // Adjust to get search terms in proper format
+      		var term = term.trim();
+      		var start = start.trim() + "0101";
+      		var end = end.trim() + "1231";
+
+		console.log("Query Run");
+		// Run a query using Axios. Return the results as an object with an array.
+		return axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+			params: {
+			    'api-key': APIKey,
+			    'q': term,
+			    'begin_date': start,
+			    'end_date': end
+			}
+		})
+		.then(function(results){
+			console.log("Axios Results", results.data.response);
+
+			return results.data.response;
+
+		});
+	},
+
+  getSaved: function(){
+		return axios.get('/api/saved')
+			.then(function(results){
+				console.log("axios results ", results);
+				return restuls;
+			})
+	},
+	postSaved: function(title, date, url){
+		var newArticle = {
+			title: title,
+			date: date,
+			url: url
+		};
+		return axios.post('/api/saved', newArticle)
+			.then(function(results){
+				console.log("acios results ", results._id);
+				return results._id;
+			})
+	},
+	deleteSaved: function(title, data, url){
+		return axios.delete('/api/saved', {
+			params: {
+				'title': title,
+				'data': data,
+				'url': url,
+			}
+		})
+		.then(function(results){
+			console.log("axios results ", results);
+			return results;
+		})
+	}
+}
+module.exports = helpers;
